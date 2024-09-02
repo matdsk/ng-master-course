858ea3cd6f1946007b760ed642a5e431c811695e
matdsk <matdsk.work@gmail.com>
2024-09-02 19:13:28 +0200

    make user reusable & add tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,12 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  output,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +15,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  // get imagePath(): string {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
-  onSelectUser = (): void => {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  };
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Output() select = new EventEmitter<string>();
+
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+  select = output<string>();
+
+  imagePath = computed<string>(() => {
+    return 'assets/users/' + this.avatar();
+  });
+
+  onSelectUser(): void {
+    this.select.emit(this.id());
+  }
 }
